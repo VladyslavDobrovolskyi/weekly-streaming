c50a0c107550287d16ade84215fcdbaa225f1009
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-13 14:05:36 +0200

    Add the changes for Initial CI-CD setup

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,17 @@
 import express from 'express'
 import path from 'path'
+import { fileURLToPath } from 'url'
+import cors from 'cors'
 
 const app = express()
-const port = 5000 // Используем порт 5000 для Express
+const port = 5555
+
+// Получаем путь к текущей директории с использованием import.meta.url
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// Настроим CORS
+app.use(cors({ origin: 'http://localhost:7777' })) // Разрешаем доступ со всех источников
 
 // Статический сервер для отдачи файлов HLS
 app.use('/stream', express.static(path.join(__dirname, '../stream')))
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,52 @@
+import React, { useEffect, useRef } from 'react'
 import ReactPlayer from 'react-player'
+import Hls from 'hls.js'
 
-function App() {
+const App: React.FC = () => {
+	// Указываем тип на ReactPlayer с ссылкой на HTMLMediaElement
+	const playerRef = useRef<ReactPlayer>(null)
+
+	useEffect(() => {
+		// Проверяем, поддерживает ли браузер HLS
+		if (Hls.isSupported() && playerRef.current) {
+			const hls = new Hls()
+			// Получаем внутренний HTMLMediaElement
+			const mediaElement = playerRef.current.getInternalPlayer() as HTMLMediaElement
+
+			if (mediaElement) {
+				// Путь к .m3u8 файлу
+				hls.loadSource('http://localhost:5555/stream/playlist.m3u8')
+				hls.attachMedia(mediaElement)
+
+				// Очистка ресурсов при размонтировании
+				return () => {
+					hls.destroy()
+				}
+			}
+		}
+	}, [])
+
 	return (
 		<div className='App'>
 			<h1>HLS Streaming Example</h1>
-			<ReactPlayer url='http://localhost:5000/stream/playlist.m3u8' controls={true} width='100%' height='auto' />
+			<ReactPlayer
+				ref={playerRef}
+				url='http://localhost:5555/stream/playlist.m3u8'
+				controls={true}
+				width='100%'
+				height='auto'
+				playing
+				config={{
+					file: {
+						attributes: {
+							crossOrigin: 'anonymous',
+						},
+						hlsOptions: {
+							startLevel: -1,
+						},
+					},
+				}}
+			/>
 		</div>
 	)
 }
