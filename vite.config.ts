d21ef8704b5d4f5720c36be6dfb7c8233ed1fd7c
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-15 20:44:36 +0200

    Refactor project structure: remove App component, update index.html and configure Vite for client directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+	root: 'client', // Set the root directory to 'client'
 	plugins: [react()],
 	server: {
 		host: '0.0.0.0',
-		port: 7777, // Задайте нужный порт
+		port: 7777, // Set the desired port
+	},
+	build: {
+		outDir: '../dist', // Output directory for the build
 	},
 })
