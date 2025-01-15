6bb5f2deb58a5402c8e89bf35360c1d2d3849eb2
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-15 20:58:18 +0200

    Refactor project structure: migrate client files to src, update TypeScript configurations, and implement VideoPlayer component

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	root: 'client', // Set the root directory to 'client'
 	plugins: [react()],
 	server: {
 		host: '0.0.0.0',
-		port: 7777, // Set the desired port
-	},
-	build: {
-		outDir: '../dist', // Output directory for the build
+		port: 7777, // Задайте нужный порт
 	},
 })
