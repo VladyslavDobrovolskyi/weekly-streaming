b30e6ce45ddb3d2e657f7af5b13377ad19bd3e0a
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-16 22:38:48 +0200

    Refactor WebSocket server import to use default import syntax

--- server/webrtc.ts
+++ server/webrtc.ts
@@ -1,6 +1,6 @@
-import { Server } from 'ws'
+import ws from 'ws'
 
-const wss = new Server({ port: 8080 })
+const wss = new ws.Server({ port: 8080 })
 
 wss.on('connection', ws => {
 	ws.on('message', message => {
