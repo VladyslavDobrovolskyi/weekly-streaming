6f79870d902a22b9d42e6c86a42eadc45ebaf776
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-16 18:02:56 +0200

    Add authentication routes to main server file

--- server/main.ts
+++ server/main.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import userRoutes from './routes/userRoutes'
 import roomReservationsRoutes from './routes/roomRoutes'
+import authRoutes from './routes/authRoutes'
 
 const app = express()
 const port = 8888
 
 app.use(express.json())
 
+app.use('/auth', authRoutes)
 app.use('/users', userRoutes)
 app.use('/room_reservations', roomReservationsRoutes)
 
