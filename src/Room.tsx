57409901af5bd8fe0614f1c91ab0cabf99e3cf14
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-18 21:20:14 +0200

    Refactor Room component: update signaling socket initialization to use WebSocket transport and simplify path

--- src/Room.tsx
+++ src/Room.tsx
@@ -58,8 +58,8 @@ const Room: React.FC = () => {
 
 		const setupSocket = (roomId: string) => {
 			console.log('Setting up socket...')
-			signalingSocketRef.current = io('https://streaming.vladyslavdobrovolskyi.tech', {
-				path: '/ws/',
+			signalingSocketRef.current = io('https://streaming.vladyslavdobrovolskyi.tech/ws', {
+				transports: ['websocket'], // Use WebSocket instead of polling
 			})
 			console.log('Signaling socket created')
 
