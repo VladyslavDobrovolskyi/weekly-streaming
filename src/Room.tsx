f3f8eab1b8a04a0710b9a51834e7bf130a0acbf6
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-18 23:04:19 +0200

    Commit

--- src/Room.tsx
+++ src/Room.tsx
@@ -20,7 +20,7 @@ const Room: React.FC = () => {
 	const [message, setMessage] = useState<string>('')
 
 	useEffect(() => {
-		const socket = io('https://streaming.vladyslavdobrovolskyi.tehc/socket.io')
+		const socket = io('https://streaming.vladyslavdobrovolskyi.tech/socket.io')
 
 		socket.on('hello', arg => {
 			console.log(arg)
