8a1b6f12870f621affe6340add5c5d598df2801a
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-18 21:14:36 +0200

    Refactor WebRTC server: remove unused Express app initialization from WebSocket server setup

--- server/webrtc.ts
+++ server/webrtc.ts
@@ -1,9 +1,7 @@
-import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
 
-const app = express()
-const server = http.createServer(app)
+const server = http.createServer()
 const io = new Server(server, {
 	cors: {
 		origin: '*',
